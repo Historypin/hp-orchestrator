77594693da8e30966ac752af8adfdabdc2d88e87
Miroslav Valus <miroslav.valus@eea.sk>
2016-04-19 11:24:48 +0200

    Added search for field 'edmIsShownBy' in items detail, when pin content is empty.

--- console/src/main/java/sk/eea/td/config/RESTClientsConfig.java
+++ console/src/main/java/sk/eea/td/config/RESTClientsConfig.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import sk.eea.td.eu_client.api.EuropeanaClient;
+import sk.eea.td.eu_client.impl.EuropeanaClientImpl;
 import sk.eea.td.hp_client.api.HPClient;
 import sk.eea.td.hp_client.impl.HPClientImpl;
 
@@ -22,5 +24,14 @@ public class RESTClientsConfig {
             @Value("${historypin.api.secret}") String apiSecret
     ) {
         return new HPClientImpl(baseURL, apiKey, apiSecret);
+    }
+
+    @Bean
+    public EuropeanaClient europeanaClient(
+            @Value("${europeana.base.url}") String baseURL,
+            @Value("${europeana.ws.key}") String wsKey,
+            @Value("${europeana.retry}") Integer maxRetries,
+            @Value("${europeana.retry.delay}") Integer retryDelay) {
+        return new EuropeanaClientImpl(baseURL, wsKey, maxRetries, retryDelay);
     }
 }
