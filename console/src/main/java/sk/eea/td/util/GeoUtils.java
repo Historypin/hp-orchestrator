4aeaa8df296ee35b76e6a73f6f89e27d55cce5e0
Miroslav Valus <miroslav.valus@eea.sk>
2016-04-20 09:50:28 +0200

    Reformates and adds documentations to GeoUtils class.

--- console/src/main/java/sk/eea/td/util/GeoUtils.java
+++ console/src/main/java/sk/eea/td/util/GeoUtils.java
@@ -1,7 +1,5 @@
 package sk.eea.td.util;
 
-import sk.eea.td.hp_client.api.Location;
-
 public class GeoUtils {
 
     public static Long EARTH_RADIUS = 6371000L;
@@ -9,7 +7,8 @@ public class GeoUtils {
     /**
      * Calculates distance in meters between two places on Earth, described by their position [latitude, longitude].
      * For calculation method uses 'haversine' formula as described <a href="http://www.movable-type.co.uk/scripts/latlong.html#ortho-dist" >here</a>.
-     * @param first first location.
+     *
+     * @param first  first location.
      * @param second second location.
      * @return distance between locations in meters.
      */
@@ -21,11 +20,19 @@ public class GeoUtils {
 
         final Double a =
                 Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2)
-                        + Math.cos(phi1) * Math.cos(phi2) *  Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
-        final Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+                        + Math.cos(phi1) * Math.cos(phi2) * Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
+        final Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return (long) (EARTH_RADIUS * c);
     }
 
+    /**
+     * Calculates middle point between two locations, described by their position [latitude, longitude].
+     * Uses formula described <a href="http://www.movable-type.co.uk/scripts/latlong.html#midpoint" >here</a>.
+     *
+     * @param first  first location.
+     * @param second second location.
+     * @return location of midpoint.
+     */
     public static Location calculateMidpoint(Location first, Location second) {
         final Double phi1 = Math.toRadians(first.getLat());
         final Double phi2 = Math.toRadians(second.getLat());
@@ -35,13 +42,22 @@ public class GeoUtils {
         final Double By = Math.cos(phi2) * Math.sin(deltaLambda);
         final Double phi3 = Math.atan2(
                 Math.sin(phi1) + Math.sin(phi2),
-                Math.sqrt(Math.pow(Math.cos(phi1) + Bx, 2)  + Math.pow(By,2))
+                Math.sqrt(Math.pow(Math.cos(phi1) + Bx, 2) + Math.pow(By, 2))
         );
         final Double lambda3 = Math.toRadians(first.getLng()) + Math.atan2(By, Math.cos(phi1) + Bx);
-        return new Location(Math.toDegrees(phi3), normalizeCoordinate(Math.toDegrees(lambda3)));
+        return new Location(Math.toDegrees(phi3), normalizeLongitudeCoordinate(Math.toDegrees(lambda3)));
     }
 
-    public static Double normalizeCoordinate(Double d) {
+    /**
+     * Normalizes longitude coordinates between (-180, +180) degrees.
+     * E.g.:
+     * <pre><code>Double d = normalizeLongitudeCoordinate(-181);</code></pre>
+     * Variable 'd' wil have value 179.0.
+     *
+     * @param d coordinates in degrees.
+     * @return normalized coordinates in degrees.
+     */
+    public static Double normalizeLongitudeCoordinate(Double d) {
         return (d + 540) % 360 - 180;
     }
 
@@ -78,7 +94,8 @@ public class GeoUtils {
             this.lng = lng;
         }
 
-        @Override public boolean equals(Object o) {
+        @Override
+        public boolean equals(Object o) {
             if (this == o)
                 return true;
             if (o == null || getClass() != o.getClass())
@@ -92,13 +109,15 @@ public class GeoUtils {
 
         }
 
-        @Override public int hashCode() {
+        @Override
+        public int hashCode() {
             int result = getLat() != null ? getLat().hashCode() : 0;
             result = 31 * result + (getLng() != null ? getLng().hashCode() : 0);
             return result;
         }
 
-        @Override public String toString() {
+        @Override
+        public String toString() {
             return "Location{" +
                     "lat=" + lat +
                     ", lng=" + lng +
