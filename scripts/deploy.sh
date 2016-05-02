cd6c33b7a213e0961c2708c13d95bea9a311e128
Miroslav Valus <miroslav.valus@eea.sk>
2016-05-02 10:03:54 +0200

    Added deploy script used by CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,54 @@
 #!/usr/bin/env bash
+
+TIMEOUT_LIMIT=100
+TOMCAT_WEBAPPS_DIR="/var/lib/tomcat8/webapps"
+CONSOLE_STATUS_CHECK="localhost:8080/console/api/status"
+
+function stop_tomcat() {
+    echo "Shutting down tomcat..."
+    sudo systemctl stop tomcat8
+    while ps aux | grep "[c]atalina.base=/var/lib/tomcat8"; do
+        sleep 1;
+    done
+
+    echo "Clearing webapps..."
+    rm -rfv ${TOMCAT_WEBAPPS_DIR}/*
+}
+
+function start_tomcat() {
+    echo "Starting tomcat..."
+    sudo systemctl start tomcat8
+
+    export http_proxy=''; # disable proxy for session
+    status="NOT OK"
+    count=0
+    count_by=5
+    while [[ (${status} != OK)  && (${count} -lt ${TIMEOUT_LIMIT}) ]]; do
+        status=`curl --silent --max-time 1 --header "content-type: application/json" ${CONSOLE_STATUS_CHECK}`
+        echo "Will wait for tomcat to start in " `expr ${TIMEOUT_LIMIT} - ${count}` " s..."
+        sleep ${count_by}
+        let count=${count}+${count_by};
+    done
+
+    if [[ ${count} -eq ${TIMEOUT_LIMIT} ]]; then
+        echo "Tomcat did not start in allowed limit!"
+        return 0;
+    fi
+
+    echo "Tomcat is up and running"
+    return 1;
+}
+
+function deploy_console() {
+    echo "Deploying console..."
+    cp -v "console.war" -t ${TOMCAT_WEBAPPS_DIR}
+}
+
+function deploy_mule() {
+    echo "Deploying mule app..."
+}
+
+stop_tomcat
+deploy_console
+start_tomcat
+deploy_mule
